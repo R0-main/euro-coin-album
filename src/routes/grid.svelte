1a8d282ba55e1c3e53f50111b66aeb8948df3214
Romain <justromixx@gmail.com>
2024-07-25 10:43:50 +0200

    ADDED colapsible

--- src/routes/grid.svelte
+++ src/routes/grid.svelte
@@ -8,29 +8,34 @@
 	const to = new Date().getFullYear() + 1;
 </script>
 
-<h1 class=" my-5 w-full justify-center text-center align-middle text-5xl font-bold">
-	<span role="img">{contry}</span>
-</h1>
-
-<div class=" flex w-full justify-center">
-	<table class="table-pin-cols table-xs text-center mr-16">
-		<thead>
-			<tr>
-				<th />
-				{#each Object.keys(euroCoins).reverse() as coin}
-					<td class=" font-bold text-2xl">{coin}</td>
-				{/each}
-			</tr>
-		</thead>
-		<tbody class="">
-			{#each { length: to - from } as _, i}
-				<tr>
-					<th class=" font-bold text-2xl">{i + from}</th>
-					{#each Object.values(euroCoins).reverse() as coinImg, y}
-						<Coin coinImg={coinImg} contry={contry} coin={Object.keys(euroCoins)[y]} year={i + from}/>
+<details class="collapse bg-base-100 hover:bg-base-200">
+	<summary class="collapse-title text-xl font-medium "
+		><h1 class="my-5 w-full justify-center text-center align-middle text-5xl font-bold ">
+			<span role="img">{contry}</span>
+		</h1>
+	</summary>
+	<div class="collapse-content">
+		<div class="flex w-full justify-center">
+			<table class="table-pin-cols table-xs mr-16 text-center">
+				<thead>
+					<tr>
+						<th />
+						{#each Object.keys(euroCoins).reverse() as coin}
+							<td class="text-2xl font-bold">{coin}</td>
+						{/each}
+					</tr>
+				</thead>
+				<tbody>
+					{#each { length: to - from } as _, i}
+						<tr>
+							<th class="text-2xl font-bold">{i + from}</th>
+							{#each Object.values(euroCoins).reverse() as coinImg, y}
+								<Coin {coinImg} {contry} coin={Object.keys(euroCoins)[y]} year={i + from} />
+							{/each}
+						</tr>
 					{/each}
-				</tr>
-			{/each}
-		</tbody>
-	</table>
-</div>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</details>
